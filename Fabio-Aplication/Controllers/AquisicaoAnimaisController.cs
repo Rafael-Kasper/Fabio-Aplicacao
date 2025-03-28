7a1826f3ff6affd436c56a7b2d038c87832c552b
Rafael-Kasper <rafa_kasper@hotmail.com>
2025-03-28 03:00:29 -0300

    Alterção no layout geral, navbar resolvido, criado crud de funcionarios e de Animais. Melhorei o layout dessas paginas porem ainda n terminei.

---
+++ Fabio-Aplication/Controllers/AquisicaoAnimaisController.cs
@@ -0,0 +1,106 @@
+﻿using Fabio_Aplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fabio_Aplication.Controllers
+{
+    public class AquisicaoAnimaisController : Controller
+    {
+
+        private BancoEntities db = new BancoEntities();
+        // GET: AquisicaoAnimais
+ 
+
+        // GET: Funcionarios
+        public ActionResult Index()
+        {
+            var animais = db.AQUISICAO_ANIMAIS.ToList();
+            return View(animais);
+        }
+
+        // GET: Funcionarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Funcionarios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AQUISICAO_ANIMAIS animais)
+        {
+            if (ModelState.IsValid)
+            {
+             
+               
+
+                db.AQUISICAO_ANIMAIS.Add(animais);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(animais);
+        }
+
+        // GET: Funcionarios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var animais = db.AQUISICAO_ANIMAIS.Find(id);
+            if (animais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animais);
+        }
+
+        // POST: Funcionarios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AQUISICAO_ANIMAIS animais)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+                db.Entry(animais).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(animais);
+        }
+
+        // GET: Funcionarios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var animais = db.AQUISICAO_ANIMAIS.Find(id);
+            if (animais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animais);
+        }
+
+        // POST: Funcionarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var animais = db.AQUISICAO_ANIMAIS.Find(id);
+            db.AQUISICAO_ANIMAIS.Remove(animais);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Fabio-Aplication/Controllers/FuncionariosController.cs
@@ -0,0 +1,101 @@
+﻿using Fabio_Aplication.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Fabio_Aplication.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        private BancoEntities db = new BancoEntities();
+
+        // GET: Funcionarios
+        public ActionResult Index()
+        {
+            var funcionarios = db.FUNCIONARIOS.ToList();
+            return View(funcionarios);
+        }
+
+        // GET: Funcionarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Funcionarios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FUNCIONARIOS funcionario)
+        {
+            if (ModelState.IsValid)
+            {
+                funcionario.DATACAD = DateTime.Now;
+                funcionario.DATAALT = DateTime.Now;
+
+                db.FUNCIONARIOS.Add(funcionario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(funcionario);
+        }
+
+        // GET: Funcionarios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var funcionario = db.FUNCIONARIOS.Find(id);
+            if (funcionario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(funcionario);
+        }
+
+        // POST: Funcionarios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(FUNCIONARIOS funcionario)
+        {
+            if (ModelState.IsValid)
+            {
+                funcionario.DATAALT = DateTime.Now;
+
+                db.Entry(funcionario).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(funcionario);
+        }
+
+        // GET: Funcionarios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var funcionario = db.FUNCIONARIOS.Find(id);
+            if (funcionario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(funcionario);
+        }
+
+        // POST: Funcionarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var funcionario = db.FUNCIONARIOS.Find(id);
+            db.FUNCIONARIOS.Remove(funcionario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
